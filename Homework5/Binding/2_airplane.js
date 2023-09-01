e077992c3dc82827ba2960a31ed71c53e25b9b50
Maria Javadyan <maria.javadyan02@gmail.com>
2023-09-02 01:35:13 +0400

    adding airplane object with takeOff and land methods

--- Homework5/Binding/2_airplane.js
+++ Homework5/Binding/2_airplane.js
@@ -1,4 +1,4 @@
-//2.  Write an Airplane object that initializes name.
+//2. Write an Airplane object that initializes name.
 // Give airplanes the ability to .takeOff() and .land():
 // If a plane takes off, its isFlying property is set to true.
 // If a plane lands, its isFlying property is set to false.
