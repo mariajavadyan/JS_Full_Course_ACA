6a8393d72c58124d021171c80d12d830a66cc21a
Maria Javadyan <maria.javadyan02@gmail.com>
2023-09-02 01:25:33 +0400

    Adding my own call method written by hand

---
+++ Homework5/Binding/4_call.js
@@ -0,0 +1,19 @@
+const person = {
+  name: "Maria",
+};
+
+function info(phone, email) {
+  console.log(`Name: ${this.name}, Phone: ${phone}, Email: ${email}`);
+}
+
+function call(fn, contextObj, ...args) {
+  let uniqeId = Date.now().toString() + Math.random();
+  contextObj[uniqeId] = fn;
+  let result = contextObj[uniqeId](...args);
+
+  delete contextObj[uniqeId];
+
+  return result;
+}
+
+call(info, person, "12345", "maria@gmail.com");
