017e3c414e2acebe56afcd38564de88ed4ab17f0
Maria Javadyan <maria.javadyan02@gmail.com>
2023-09-30 09:02:53 +0400

    Adding function that returns name of month accortingly to number

---
+++ Homework9/getMonthName_3.js
@@ -0,0 +1,18 @@
+function monthName(num) {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  return months[num - 1];
+}
